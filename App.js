024d453bf04e9b2505d8db63205d9578a7a2e91c
whos-bax <loot04@naver.com>
2022-10-24 22:47:16 +0900

    drawer

--- App.js
+++ App.js
@@ -6,15 +6,20 @@
  * @flow strict-local
  */
 
+// import 'react-native-gesture-handler';
 import React, {Component} from 'react';
 import {StyleSheet, View, Text, Image, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from './src/home';
 import UserScreen from './src/user';
 import LogoTitle from './src/logo';
+import DrawerHomeScreen from './src/home_drawer';
+import DrawerUserScreen from './src/user_drawer';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 class App extends Component {
   logoTitle = () => {
@@ -30,6 +35,12 @@ class App extends Component {
   };
   render() {
     return (
+      // <NavigationContainer>
+      //   <Drawer.Navigator>
+      //     <Drawer.Screen name="Home" component={DrawerHomeScreen} />
+      //     <Drawer.Screen name="User" component={DrawerUserScreen} />
+      //   </Drawer.Navigator>
+      // </NavigationContainer>
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="Home"
---
+++ src/home_drawer.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React, {Component} from 'react';
+ import {StyleSheet, View, Text, Button} from 'react-native';
+ 
+ class DrawerHomeScreen extends Component {
+   render() {
+     return (
+       <View
+         style={{
+           flex: 1,
+           alignItems: 'center',
+           justifyContent: 'center',
+         }}>
+         <Text>Home Screen</Text>
+         <Button
+           title="To User Screen"
+           onPress={() => {
+             this.props.navigation.navigate('User');
+           }}
+         />
+       </View>
+     );
+   }
+ }
+ 
+ const styles = StyleSheet.create({});
+ 
+ export default DrawerHomeScreen;
+ 
